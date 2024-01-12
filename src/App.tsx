0132fd9d294232a07e03b2a83a40712b85875d58
Hrithik <slumdog4321@gmail.com>
2024-01-12 11:46:59 +0530

    added userinput component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { faker } from "@faker-js/faker";
 import RestartButton from "./components/RestartButton";
 import Results from "./components/Results";
+import UserTypings from "./components/UserTypings";
 
 const words = faker.lorem.words(10);
 
@@ -8,15 +9,24 @@ function App() {
   return (
     <>
       <CountdownTimer timeleft={30} />
-      <GenarateRandomWords words={words} />
+      <WordsContainer>
+        <GenarateRandomWords words={words} />
+        <UserTypings className="absolute inset-0" userInput={"test"} />
+      </WordsContainer>
       <RestartButton className={"mx-auto mt-10 text-slate-500"} onRestart={() => null} />
       <Results className="mt-10" accuracyPercentage={100} errors={20} total={200} />
     </>
   );
 }
 
+const WordsContainer = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="relative text-3xl max-w-xl leading-relaxed break-all mt-3">{children}</div>
+  );
+};
+
 const GenarateRandomWords = ({ words }: { words: string }) => {
-  return <div className="text-4xl text-center text-slate-500">{words}</div>;
+  return <div className=" text-slate-500">{words}</div>;
 };
 
 const CountdownTimer = ({ timeleft }: { timeleft: number }) => {
---
+++ src/components/UserTypings.tsx
@@ -0,0 +1,17 @@
+const UserTypings = ({ userInput, className = "" }: { userInput: string; className?: string }) => {
+  const typedCharacters = userInput.split("");
+
+  return (
+    <div className={className}>
+      {typedCharacters.map((char, index) => (
+        <Character key={`${char}_${index}`} char={char} />
+      ))}
+    </div>
+  );
+};
+
+const Character = ({ char }: { char: string }) => {
+  return <span className="text-primary">{char}</span>;
+};
+
+export default UserTypings;
