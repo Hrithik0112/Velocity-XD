fcf9c0ff267efe9ac57568abc5ba931d0dc3ea1d
Hrithik <slumdog4321@gmail.com>
2024-01-12 11:10:09 +0530

    countdown timer componnet added

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,20 @@ import { faker } from "@faker-js/faker";
 const words = faker.lorem.words(10);
 
 function App() {
-  return <GenarateRandomWords words={words} />;
+  return (
+    <>
+      <CountdownTimer timeleft={30} />
+      <GenarateRandomWords words={words} />
+    </>
+  );
 }
 
 const GenarateRandomWords = ({ words }: { words: string }) => {
   return <div className="text-4xl text-center text-slate-500">{words}</div>;
+};
+
+const CountdownTimer = ({ timeleft }: { timeleft: number }) => {
+  return <h2 className="text-primary font-medium">Time : {timeleft}</h2>;
 };
 
 export default App;
